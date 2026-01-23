78df46f354c0a4bc602bd8e97b9922b5bbc11707
yksamm <blissthetic04@gmail.com>
2026-01-23 15:24:07 +0530

    removed chatgpt code

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function addTask() {
-  const input = document.getElementById("taskInput");
-  const taskList = document.getElementById("taskList");
 
-  if (input.value === "") return;
-
-  const li = document.createElement("li");
-  li.textContent = input.value;
-
-  li.onclick = () => li.classList.toggle("done");
-
-  taskList.appendChild(li);
-  input.value = "";
-}
